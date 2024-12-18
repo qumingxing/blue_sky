517ab3ac7a04bf51e736aebdb952535e748fac9c
qumingxing <qmx_000@163.com>
2024-12-18 15:28:33 +0800

    [RS-1001] parse http protocol

--- blue_sky_web/src/main.rs
+++ blue_sky_web/src/main.rs
@@ -1,5 +1,15 @@
+use flexi_logger::{AdaptiveFormat, Duplicate, Logger};
+
 mod server;
 
 fn main() {
+    Logger::with_env_or_str("info")
+        .adaptive_format_for_stderr(AdaptiveFormat::Detailed)
+        .adaptive_format_for_stdout(AdaptiveFormat::Default)
+        .log_to_file()
+        .duplicate_to_stderr(Duplicate::Info)
+        .start()
+        .unwrap();
+
     server::start_server();
 }
--- blue_sky_web/src/server.rs
+++ blue_sky_web/src/server.rs
@@ -1,6 +1,9 @@
-use std::{env, fs};
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::{env, fs};
 
 pub fn start_server() {
     let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
@@ -15,25 +18,123 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
-
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let http_protocol = &parse_protocol(&buffer);
+    println!("{:?}", http_protocol.request_value.get_int("b"));
+    let dir = env::current_dir().unwrap();
+    let html = format!("{}{}", dir.to_str().unwrap(), "/blue_sky_web/hello.html");
+    let contents = fs::read_to_string(html).unwrap();
 
-    let get = b"GET / HTTP/1.1\r\n";
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
 
-    if buffer.starts_with(get) {
-        let dir = env::current_dir().unwrap();
-        let html = format!("{}{}", dir.to_str().unwrap(), "/blue_sky_web/hello.html");
-        let contents = fs::read_to_string(html).unwrap();
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+enum Method {
+    GET,
+    POST,
+    DELETE,
+}
+impl Default for Method {
+    fn default() -> Self {
+        Method::GET
+    }
+}
+#[derive(Debug, PartialEq, Eq)]
+struct HttpProtocol {
+    context_path: String,
+    method: Method,
+    headers: HashMap<String, String>,
+    request_value: RequestValue,
+}
+#[derive(Debug, PartialEq, Eq)]
+struct RequestValue {
+    request_params: HashMap<String, String>,
+}
+impl RequestValue {
+    fn new(request_params: HashMap<String, String>) -> RequestValue {
+        RequestValue { request_params }
+    }
+    fn get_int(&self, key: &str) -> Option<i32> {
+        match self.request_params.get(key) {
+            Some(value) => Some(value.parse::<i32>().unwrap()),
+            None => None,
+        }
+    }
 
-        let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-            contents.len(),
-            contents
-        );
+    fn get_string(&self, key: &str) -> Option<String> {
+        match self.request_params.get(key) {
+            Some(value) => Some(value.to_string()),
+            None => None,
+        }
+    }
+    fn get_float(&self, key: &str) -> Option<f32> {
+        match self.request_params.get(key) {
+            Some(value) => Some(value.parse::<f32>().unwrap()),
+            None => None,
+        }
+    }
+    fn get_double(self, key: &str) -> Option<f64> {
+        match self.request_params.get(key) {
+            Some(value) => Some(value.parse::<f64>().unwrap()),
+            None => None,
+        }
+    }
+    fn get_bool(&self, key: &str) -> Option<bool> {
+        match self.request_params.get(key) {
+            Some(value) => Some(value.parse::<bool>().unwrap()),
+            None => None,
+        }
+    }
+}
+fn parse_protocol(buffer: &[u8; 1024]) -> HttpProtocol {
+    let original_content = String::from_utf8_lossy(&buffer[..]);
+    let raw_lines = original_content.split("\n").collect::<Vec<&str>>();
+    let request_method_params = raw_lines[0].split(" ").collect::<Vec<&str>>();
+    let method = match request_method_params[0] {
+        "GET" => Method::GET,
+        "POST" => Method::POST,
+        "DELETE" => Method::DELETE,
+        _ => Method::default(),
+    };
 
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+    let context_path_params = request_method_params[1];
+    let context_path = if let Some(end_pos) = context_path_params.find("?") {
+        request_method_params[1][0..end_pos].to_string()
     } else {
-        // other requests
+        "/".to_string()
+    };
+    let regex = Regex::new(r"[?/]").unwrap();
+    let params = regex.replace_all(context_path_params, "");
+    let mut params = params.split("&").collect::<Vec<&str>>();
+
+    let mut request_params = HashMap::new();
+    for param in params {
+        let p = param.split("=").collect::<Vec<&str>>();
+        request_params.insert(p[0].to_string(), p[1].to_string());
+    }
+
+    let request_value = RequestValue::new(request_params);
+
+    let mut headers = HashMap::new();
+    for (index, value) in raw_lines.iter().enumerate().skip(1) {
+        let request_entry = value.split(": ").collect::<Vec<&str>>();
+        if request_entry.len() == 2 {
+            let key = request_entry[0].trim().to_string();
+            let value = request_entry[1].trim().to_string();
+            headers.insert(key, value);
+        }
+    }
+
+    HttpProtocol {
+        method,
+        context_path,
+        headers,
+        request_value,
     }
 }
