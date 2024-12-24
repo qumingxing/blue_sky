9a53787e54f91dfd5d2e6c99d16afee3bfe4b0d8
qumingxing <qmx_000@163.com>
2024-12-24 18:58:57 +0800

    [RS-1001] bug fixed for invalided method

--- blue_sky_web/src/request_mapping.rs
+++ blue_sky_web/src/request_mapping.rs
@@ -3,5 +3,5 @@ use crate::web_handler::WebHandler;
 pub fn register_request_mapping(){
     let router = &mut crate::server::INSTANCE.lock().unwrap();
     WebHandler::register_route_handle_demo(router);
-    WebHandler::register_route_handle_demo_x(router);
+    WebHandler::register_route_handle_demo_other(router);
 }
